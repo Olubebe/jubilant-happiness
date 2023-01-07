145d1ff9631ba7fd855869b7eb2ce8bcc6c52d27
Olubebe <faitholuwibe@gmail.com>
2023-01-07 11:36:02 +0100

    add firebase hosting

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC9bmmeQarntNOkdvH2Q7YJ9Mdrpjm_9Pk",
-  authDomain: "altpro-2d627.firebaseapp.com",
-  projectId: "altpro-2d627",
-  storageBucket: "altpro-2d627.appspot.com",
-  messagingSenderId: "405728453238",
-  appId: "1:405728453238:web:e37055d93fb2dd4ae23557",
+  apiKey: "AIzaSyDT0KU-WoZQkMXeCP6s0lJ_3J5kVKMBse0",
+  authDomain: "better-pro.firebaseapp.com",
+  projectId: "better-pro",
+  storageBucket: "better-pro.appspot.com",
+  messagingSenderId: "925520367655",
+  appId: "1:925520367655:web:9ec8fc660236c52fa27e02",
 };
 
 // Initialize Firebase
